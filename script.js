eb85d91a53f5f3921529d06b35aa9be67fd3c327
teysuzin <pablomonteirodasilva32@gmail.com>
2025-10-30 19:22:49 -0300

    annoying dog music function

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("ano").textContent = new Date().getFullYear();
+
+  const img = document.getElementById("audio-container");
+  const music = document.getElementById("audio-file");
+
+  img.addEventListener("click", () => {
+    if (music.paused) {
+      music.play();
+    } else {
+      music.pause();
+    }
+  });
 });
