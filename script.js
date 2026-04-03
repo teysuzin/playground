229ba390a385dcc069b0ef32aea9971cd855d2c8
teysuzin <pablomonteirodasilva32@gmail.com>
2026-04-03 17:25:47 +0000

    inclusao-dos-principais-arquivos

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("ano").textContent = new Date().getFullYear();
+
+  const img = document.getElementById("audio-container");
+  const music = document.getElementById("audio-file");
+
+  music.loop = true;
+
+  img.addEventListener("click", () => {
+    if (music.paused) {
+      music.play();
+    } else {
+      music.pause();
+    }
+  });
+
+  twemoji.parse(document.body);
+});
